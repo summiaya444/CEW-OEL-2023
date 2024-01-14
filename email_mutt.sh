2747ffca31b32b81d5fef15a7f84ffd7634445d9
Irza Salahuddin <130214670+Irza-Salahuddin@users.noreply.github.com>
2024-01-14 15:19:38 +0500

    Update email_mutt.sh

--- email_mutt.sh
+++ email_mutt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GMAIL_USERNAME="irzasalahuddin27@gmail.com"
-GMAIL_PASSWORD="apjf wmkx llhm ljsg"
+GMAIL_PASSWORD="apjfwmkxllhmljsg"
 TO_ADDRESS="summiaya3fatimah@gmail.com"
 SUBJECT="Weekly Weather Report"
 BODY_FILE="weekly_report.txt"
@@ -11,6 +11,6 @@ if [ ! -f "weekly_report.txt" ]; then
     echo "Error: weekly_report.txt not found."
     exit 1
 fi
-
+chmod +x "send_email_mutt.sh"
 # Use mutt to send the email
 mutt -s "$SUBJECT" "$TO_ADDRESS" -e "set smtp_url=smtps://irzasalahuddin27@gmail.com:apjfwmkxllhmljsg@smtp.gmail.com:465" -a "$BODY_FILE" < "$BODY_FILE"
